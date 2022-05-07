f38b19dd7541af9e868cde271566184d2225573b
Филимонов Павел <99137277+painerit@users.noreply.github.com>
2022-05-07 23:07:42 +0300

    Филимонов Павел 21-ИСТ-3

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,60 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+Train::Cage* Train::create(bool light) {
+  Cage* cage = new Cage;
+  cage->light = light;
+  cage->next = nullptr;
+  cage->prev = nullptr;
+  return cage;
+}
+
+void Train::addCage(bool light) {
+  if (last && first) {
+    last->next = create(light);
+    last->next->prev = last;
+    last = last->next;
+    first->prev = last;
+    last->next = first;
+  } else {
+    first = create(light);
+    last = first;
+  }
+}
+
+int Train::getLength() {
+  int lgth = 0;
+  first->light = true;
+  Cage* tmp = first;
+  while (true) {
+    lgth++;
+    for (int i = 0; i < lgth; ++i) {
+      tmp = tmp->next;
+      ++countOp;
+  }
+    if (tmp->light) {
+      tmp->light = false;
+      for (int i = 0; i < lgth; ++i) {
+        tmp = tmp->prev;
+        countOp += 1;
+      }
+    } else {
+      while (!tmp->light) {
+        lgth++;
+        tmp = tmp->next;
+        countOp += 1;
+      }
+      tmp->light = false;
+      for (int i = 0; i < lgth; ++i) {
+        tmp = tmp->prev;
+        countOp++;
+      }
+    }
+    if (!tmp->light)
+      break;
+    }
+  return lgth;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
